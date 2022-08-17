22d3ec57bd734185e19cd24359b26138ec4108ff
Arun George <arungeorge101@gmail.com>
2022-08-17 19:25:02 -0400

    itc hcnages

--- pagsales.py
+++ pagsales.py
@@ -8,11 +8,16 @@ toDate = sys.argv[2]
 if(os.path.exists('/Users/arungeorge/Documents/Personal/PAGeorgeCo/2022/summary.xlsx')):
     os.remove('/Users/arungeorge/Documents/Personal/PAGeorgeCo/2022/summary.xlsx')
 
-companyList = ['PAG-EKM','PAG-ALPY','Pages-Ekm','PAG-agy-EKM','PAG-Calicut']
+companyList = ['PAG-EKM','PAG-ALPY','Pages-Ekm','PAG-Calicut']
 writer = pd.ExcelWriter('/Users/arungeorge/Documents/Personal/PAGeorgeCo/2022/summary.xlsx', engine='xlsxwriter')
 
 for singleCompany in companyList:
+    
     daybookDF = pd.read_excel('/Users/arungeorge/Documents/Personal/PAGeorgeCo/2022/PAG_dayBook.xlsx',sheet_name=singleCompany)
+
+    #combine the sales for ITC (pages and Agencies)
+    if(singleCompany == "Pages-Ekm"):
+        daybookDFAgy = pd.read_excel('/Users/arungeorge/Documents/Personal/PAGeorgeCo/2022/PAG_dayBook.xlsx',sheet_name="PAG-agy-EKM")
 
     dateRange = pd.date_range(fromDate, toDate)
 
@@ -23,6 +28,7 @@ for singleCompany in companyList:
 
     for singleDate in dateRange:
         filter_dayBook = daybookDF[(daybookDF['Date']>= singleDate) & (daybookDF['Date'] <= singleDate)]
+
         daySale = filter_dayBook['Amount'].sum()
         dayProfit = filter_dayBook['Profit'].sum()
 
@@ -30,8 +36,21 @@ for singleCompany in companyList:
         totalSale.append(round(daySale,2))
         totalProfit.append(round(dayProfit,2))
 
+        if(singleCompany == "Pages-Ekm"):
+            filter_dayBookAgy = daybookDFAgy[(daybookDFAgy['Date']>= singleDate) & (daybookDFAgy['Date'] <= singleDate)]
+            daySale = filter_dayBookAgy['Amount'].sum()
+            dayProfit = filter_dayBookAgy['Profit'].sum()
+
+            days.append(singleDate.strftime("%m-%d-%y"))
+            totalSale.append(round(daySale,2))
+            totalProfit.append(round(dayProfit,2))
+
     consolidated_df = pd.DataFrame(list(zip(days,totalSale,totalProfit)), columns=columns)
-    consolidated_df.to_excel(writer,sheet_name=singleCompany)
+    
+    if(singleCompany == "Pages-Ekm"):
+        consolidated_df.to_excel(writer,sheet_name="ITC", index=False)
+    else:
+        consolidated_df.to_excel(writer,sheet_name=singleCompany, index=False)
 
 writer.save()
 
